5bc8bb41a17136615c3ce0c976c09c5dcb8f7282
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-03-08 21:07:12 -0800

    Experiment with removing the While loop

--- tweet_bot.py
+++ tweet_bot.py
@@ -14,15 +14,15 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_key_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-interval = 60
+# interval = 60
 
-while True:
-  print("about to tweet a lyric")
+# while True:
+# print("about to tweet a lyric")
   # fetch a tweet from the api
-  tweet = requests.get(url = "https://selling-sunset-lyrics-api.herokuapp.com/api/v1/lyrics/random").json()
+tweet = requests.get(url = "https://selling-sunset-lyrics-api.herokuapp.com/api/v1/lyrics/random").json()
   
   # tweet
-  api.update_status(tweet)
-  time.sleep(interval)
+api.update_status(tweet)
+  # time.sleep(interval)
 
 
